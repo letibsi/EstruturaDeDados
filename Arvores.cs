c373c5338e0b798dbde1a84cf372041130f986e9
Letícia Castro <letiscastros@gmail.com>
2025-03-18 16:43:20 -0300

    Update Arvores.cs

--- Arvores.cs
+++ Arvores.cs
@@ -1,1 +1,20 @@
+//Árvores binárias
+//Classe que cria a estrutura de nó da árvore.
 
+Class BTreeNo
+{
+      private int valor;
+      private BTreeNo esq;
+      private BTreeNo dir;
+
+      BTreeNo(int valor)
+      {
+          this.valor = valor;
+      }
+      public void setValor(int valor)
+      public void setEsc(BTreeNo esq)
+      public void setDir(BTreeNo dir)
+      public int getValor()
+      public BTreeNo getEsc()
+      public BTreeNo getDir()
+}
