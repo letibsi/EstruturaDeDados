79b44b060519e3a158bc0f3fb53254e826b731f1
Letícia Castro <letiscastros@gmail.com>
2025-03-18 15:26:22 -0300

    Update ListasOrdenadas.cs

--- ListasOrdenadas.cs
+++ ListasOrdenadas.cs
@@ -1,1 +1,25 @@
+//Método de inserção em uma lista ordenada encadeada.
+
+void insereNoOrdenado(Node novo)
+{
+  Node aux;
+  //Verificando se a lista está vazia
+  if (primeiro == null || contaNos() == 0)
+  {
+      novo.setProx(primeiro);
+      primeiro = novo;
+  }
+  else
+  {
+      aux = primeiro;
+      int valorNovo = Convert.ToInt32(novo.getItem());
+      int valorAux = Convert.ToInt32(aux.getItem());
+      while (aux.getProx() != null && valorNovo > valorAux)
+      {
+          aux = aux.getProx();
+      }
+      novo.setProx(aux.getProx());
+      aux.setProx(novo);
+  }
+}
 
